a6dfcb246aeb0feaa5480b30babf586484d03bdd
Ari Angga Nugraha <ari.angga@ui.ac.id>
2021-03-10 08:33:56 +0700

    add loading

--- src/components/Kalkulator/Kalkulator.js
+++ src/components/Kalkulator/Kalkulator.js
@@ -7,7 +7,9 @@ export default class Kalkulator extends Component {
         b: 0,
         tipe: '',
         ans: 0,
+        loading: false
     };
+
 
     handleChange = (event) => {
         this.setState({
@@ -17,19 +19,31 @@ export default class Kalkulator extends Component {
     };
 
     onClickButton = (tipe) => {
+        this.setState({
+            loading: true
+        });
         const hitung = {
-            ...this.state,
+            a: this.state.a,
+            b: this.state.b,
+            ans: this.state.ans,
             tipe: tipe
-        }
+        };
+
         axios.post('https://arinugraha.herokuapp.com/kabataku', hitung)
             .then(response => {
                 this.setState({
-                    ans: response.data['ans']
+                    ans: response.data['ans'],
+                    loading: false
                 })
             })
     }
 
     render() {
+        let loading = null;
+        if (this.state.loading) {
+            loading = <p>Loading</p>;
+        };
+
         return (
             <div>
                 {this.state.ans}
@@ -41,6 +55,8 @@ export default class Kalkulator extends Component {
                 <button onClick={() => this.onClickButton('-')}>-</button>
                 <button onClick={() => this.onClickButton('*')}>*</button>
                 <button onClick={() => this.onClickButton('/')}>/</button>
+                <br />
+                {loading}
             </div>
         );
     }
